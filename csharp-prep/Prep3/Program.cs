c23100b45cd6378e7eb527e433546bd2e820a6d2
Rebekkah Stakebake <relsoccer4@gmail.com>
2023-04-22 23:57:51 -0400

    loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        // Console.WriteLine("What is your magic number? ");
+        // string number = Console.ReadLine();
+
+        Random randomGenerator = new Random ();
+        int magic = randomGenerator.Next (1, 101);
+
+        // int magic = int.Parse(number);
+        int guess = -1;
+
+        while (guess != magic)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess =  int.Parse(Console.ReadLine());
+
+            
+            if (guess > magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magic)
+            {
+                Console.WriteLine("Higher");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
